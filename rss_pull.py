1cbda14187ab6d5c81b962374508e193d279afa0
bmart18 <mbrad664@gmail.com>
2021-04-21 14:19:12 -0400

    Added Support for Multi-Email list
    Added input from command line to handle:
         default
         specific one email
         email list (txt file separated by commas)
    
    Increased efficiency in send email to reduce time when sending to a long list
    
    Added .gitignore to ignore log file and text files such as an email list.

--- rss_pull.py
+++ rss_pull.py
@@ -1,5 +1,7 @@
 import datetime
 import logging
+import os
+
 import yagmail
 import feedparser
 
@@ -7,14 +9,47 @@ logging.basicConfig(level=logging.INFO,
                     format='%(asctime)s %(name)s %(levelname)-4s : %(message)s',
                     filename='main.log',filemode='w')
 
+def start():
+    scraper = Scraper()
+    scraper.scrub()
+    scraper.build_file()
+    get_info()
+
+
 def send_email(user_email):
     '''Sends completed file to passed email address at the end of runtime'''
     receiver=user_email
     body = "News | Blog Posts | Articles on Python \n brought to you by Odin May"
     filename = str(datetime.date.today()) + '.txt'
-    yag=yagmail.SMTP('odinmerlinmay@gmail.com')
-    yag.send(to=receiver,subject="Odins Python Newsletter",contents=body,attachments=filename)
-    logging.info("Email Sent")
+    yag=yagmail.SMTP('mbrad664@gmail.com')
+    if(isinstance(receiver,list)):
+        for emails in receiver:
+            yag.send(to=emails,subject="Odins Python Newsletter",contents=body,attachments=filename)
+            logging.info("Email Sent to " + emails)
+    else:
+        yag.send(to=receiver, subject="Odins Python Newsletter", contents=body, attachments=filename)
+        logging.info("Email Sent to " + receiver)
+
+
+
+def get_info():
+    email = input("Please Enter the Email(s) You Want to Send RSS or Enter 100 for default: ")
+    input_type = email[-3:]
+    #print(input_type) remove comment for test
+    if input_type == "com":
+        send_email(email)
+    elif input_type == "txt":
+        while not os.path.isfile(email):
+            email = input("Whoops! No such file! Please enter the name of the file you'd like to use.")
+        my_file = open(email, "r")
+        content = my_file.read()
+        content_list = content.split(",")
+        send_email(content_list)
+    elif input_type == "100":
+        send_email("mbrad664@gmail.com")
+    else:
+        logging.info("Error Obtaining Input")
+
 
 class Scraper:
     def __init__(self):
@@ -50,7 +85,4 @@ class Scraper:
         logging.info('File Created')
 
 if __name__ == '__main__':
-    scraper = Scraper()
-    scraper.scrub()
-    scraper.build_file()
-    send_email('odinmerlinmay@gmail.com')
+    start()
